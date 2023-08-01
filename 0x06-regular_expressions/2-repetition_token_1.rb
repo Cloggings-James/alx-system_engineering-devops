724ee998a2457505383ac6919df6a18e0ed0743b
Cloggings-James <James.wanjiru.sheba@gmail.com>
2023-08-01 05:28:34 -0700

    joan

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -3,8 +3,8 @@
 # Get the input string from the command line argument
 input_string = ARGV[0]
 
-# Use regular expression to find occurrences of 'hbtn' with one or more 't's
-match_result = input_string.scan(/hbt*n/)
+# Use regular expression to find occurrences of 'hbttn', 'hbn', 'hbtn', 'hbtttn', 'hbttnn', etc. (multiple 't's)
+match_result = input_string.scan(/hb(t+)n/)
 
 # Print the matched result
 puts match_result.join
