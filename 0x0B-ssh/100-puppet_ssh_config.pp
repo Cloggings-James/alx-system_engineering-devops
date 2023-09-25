ad12e147310666fb1aa95f66fcac582814b70b28
cloggings-James <james.wanjiru.sheba@gmail.com>
2023-09-25 08:28:38 -0700

    val

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
-# set up your client SSH configuration file so
-# that you can connect to a server without typing a password.
+# Setting up my client config file
+include stdlib
 
-file_line {'PasswordAuthentication':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^PasswordAuthentication',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line {'IdentityFile':
+file_line { 'Delare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
-  match  => '^IdentityFile',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
