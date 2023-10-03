bd2439a99de9316541155068e837ac17e8f01e93
cloggings-James <james.wanjiru.sheba@gmail.com>
2023-10-03 05:24:23 -0700

    val

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,11 +1,9 @@
+#!/usr/bin/env bash
+#task advance with pupppet
 exec { 'http header':
-  command  => "sudo apt-get update -y; \
-               sudo apt-get install nginx -y; \
-               sudo sed -i '/server_name _/a add_header X-Served-By $hostname;' /etc/nginx/sites-available/default; \
-               sudo service nginx restart",
-  provider => shell,
-  path     => ['/usr/bin', '/bin'],  # Specify the PATH environment variable
-  user     => 'root',                # Specify the user running the commands
-  group    => 'root',                # Specify the group for the commands
+	command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+	provider => shell,
 }
-
